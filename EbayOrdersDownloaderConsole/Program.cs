192c80cb2b13eb9cef4c71fae5480ed750e34e5d
Alex Maslakov <gilded.honour@gmail.com>
2013-01-03 21:38:17 +0700

    filling the classes for saving in db

--- EbayOrdersDownloaderConsole/Program.cs
+++ EbayOrdersDownloaderConsole/Program.cs
@@ -34,22 +34,130 @@ namespace EbayOrdersDownloaderConsole
             using (var context = new OrderManagerEntities())
             {
                 GetOrdersCall getOrdersApiCall = new GetOrdersCall(_apiContext);
-                OrderTypeCollection orders = getOrdersApiCall.GetOrders(new TimeFilter { TimeFrom = DateTime.Now.AddDays(-111), TimeTo = DateTime.Now }, TradingRoleCodeType.Seller, OrderStatusCodeType.Active /*TODO*/);
+                OrderTypeCollection orders = getOrdersApiCall.GetOrders(
+                        new TimeFilter { TimeFrom = DateTime.Now.AddDays(-111), TimeTo = DateTime.Now },
+                        TradingRoleCodeType.Seller,
+                        OrderStatusCodeType.Completed
+                    );
                 foreach (OrderType order in orders)
                 {
                     //if not exist in db then save the order to db
                     if (context.HeaderRecords.SingleOrDefault(x => x.OrderID == order.OrderID) == null)
                     {
-                        GetItemCall getItemApiCall = new GetItemCall(_apiContext);
-                        getItemApiCall.IncludeItemCompatibilityList = true;
-                        getItemApiCall.IncludeItemSpecifics = true;
-
                         var fetchedItem = new ItemType();
                         fetchedItem.CategoryMappingAllowed = true;
                         fetchedItem.ProductListingDetails = new ProductListingDetailsType();
                         fetchedItem.ProductListingDetails.IncludePrefilledItemInformation = true;
                         string orderID = order.OrderID.Substring(0, order.OrderID.IndexOf("-"));
+                        GetItemCall getItemApiCall = new GetItemCall(_apiContext);
+                        getItemApiCall.IncludeItemCompatibilityList = true;
+                        getItemApiCall.IncludeItemSpecifics = true;
                         fetchedItem = getItemApiCall.GetItem(orderID);
+                        
+                        #region Header record
+                        var headerRecord = new HeaderRecords
+                        {
+                            OrderID = order.OrderID,
+                            InvoiceID = order.ShippingDetails.SellingManagerSalesRecordNumber.ToString(),
+                            OrderDate = order.ExternalTransaction[0].ExternalTransactionTime.ToShortDateString(), //? + todo!
+                            Email = order.TransactionArray[0].Buyer.Email, //? invalid request
+                            ShopperID = null, 
+                            BilltoFirstName = order.ShippingAddress.Name,
+                            BilltoLastName = order.ShippingAddress.Name,
+                            BilltoCompanyName = order.BuyerUserID,
+                            BilltoStreetAddress = null,
+                            BilltoOptionalAddress = null,
+                            BilltoCity = null,
+                            BilltoState = null,
+                            BilltoZip = null,
+                            BilltoCountry = null,
+                            BilltoRegion = null,  
+                            BilltoPhone = null,
+                            DeliveryMethod = order.ShippingServiceSelected.ShippingService == "ShippingMethodStandard" ? "1" : "5",
+                            ProductSubtotal = "", //?
+                            ShippingAndHandling = order.ShippingServiceSelected.ShippingServiceCost.Value.ToString(),
+                            TaxMultiplier = null, //?
+                            Tax = order.ShippingDetails.SalesTax.SalesTaxAmount.Value.ToString(),
+                            Discount = "0",
+                            OverallTotal = order.Total.Value.ToString(), //?
+                            CreditCardType = "",
+                            CreditCardNumber = "",
+                            CreditCardExpiration = "",
+                            CreditCardSecurityNumberCCV = "",
+                            NameOnCreditCard = "",
+                            ShiptoFirstName = order.ShippingAddress.Name,
+                            ShiptoLastName = order.ShippingAddress.Name,
+                            ShiptoCompanyName = order.BuyerUserID,
+                            ShiptoStreetAddress = order.ShippingAddress.Street1,
+                            ShiptoOptionalAddress = order.ShippingAddress.Street2,
+                            ShiptoCity = order.ShippingAddress.CityName,
+                            ShiptoState = order.ShippingAddress.StateOrProvince,
+                            ShiptoZip = order.ShippingAddress.PostalCode,
+                            ShiptoCountry = order.ShippingAddress.CountryName,
+                            ShiptoRegion = "", //?
+                            ShiptoPhone = order.ShippingAddress.Phone,
+                            SHOPCOMCatalogID = null,
+                            CatalogName = "EBAY",
+                            MultiplePaymentsQuantity = "", //?
+                            CanSellNamePrivacyFlag = "", //?
+                            CanSendOffersPrivacyFlag = "", //?
+                            ShopperComments = "", //?
+                            Source = "EBAY",
+                            GiftMessage = "", //?
+                            Commision = order.ExternalTransaction[0].FeeOrCreditAmount.Value.ToString() //each of the Item FinalValueFee
+                        };
+                        context.HeaderRecords.AddObject(headerRecord);
+                        #endregion
+
+                        var detailRecord = new DetailRecord
+                        {
+                            InvoiceID = order.OrderID,
+                            PurchaseID = order.TransactionArray[0].ShippingDetails.SellingManagerSalesRecordNumber.ToString(),
+                            VolumeID = null,
+                            VolumeName = null,
+                            SourceCode = order.TransactionArray[0].Item.Site.ToString(),
+                            ProductSKUCode = fetchedItem.SKU, 
+                            ProductDescription = order.TransactionArray[0].Item.Title,
+                            Quantity = order.TransactionArray[0].QuantityPurchased,
+                            UnitPrice = order.TransactionArray[0].TransactionPrice.Value,
+                            ExtendedPrice = order.TransactionArray[0].TransactionPrice.Value * order.TransactionArray[0].QuantityPurchased,
+                            CouponCodes = "",//?
+                            I_StatusCode = null,//?
+                            I_ShipDate = null,//?
+                            I_Tracking = "",//?
+                            I_ShippingMethod = null,//?
+                            I_SyncWithShop = null,//?
+                            I_OriginalCost = null,//?
+                            I_OriginalQTY = null,//?
+                            Commision = null//?
+                        };
+                        context.DetailRecord.AddObject(detailRecord);
+
+                        var orderStatus = new OrderStatus
+                        {
+                            OrderID = order.OrderID,
+                            Status = 1
+                        };
+
+                        var orderMessage = new OrderMessage
+                        {
+                            InvoiceID = order.OrderID,
+                            To = "",//?
+                            From = "",//?
+                            Message = "",//?
+                            DeliveryDate = ""//?
+                        };
+                        context.OrderMessage.AddObject(orderMessage);
+
+                        //does it have to be used and changed?
+                        //var status = new Status
+                        //{
+
+                        //};
+
+                        context.OrderStatus.AddObject(orderStatus);
+                        context.SaveChanges();
+                        context.AcceptAllChanges();
                     }
                 }
 
